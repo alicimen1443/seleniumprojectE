f057b732cacc97d317f31191c241f823fd5063ed
alicimen <alicimen1443@gmail.com>
2022-05-31 21:40:43 +0300

    day9 Driver.java

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -1,6 +1,11 @@
 package com.cydeo.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
@@ -19,4 +24,49 @@ public class Driver {
     We make it static because we will use it in a static method.
      */
     private static WebDriver driver;
+
+    /*
+    Create re-usable utility method which will return same driver instance when we call it
+
+     */
+    public static WebDriver getDriver(){
+
+        if(driver==null){
+
+            /*
+        We read our browserType from configuration.properties.
+        This way, we can control which browser is opened from outside our code, from configuration.properties
+         */
+            String browserType = ConfigurationReader.getProperty("browser");
+
+            /*
+            Depending on the browserType that will be return from configuration.properties file
+            switch statement will determine the case, and open the matching browser
+             */
+            switch (browserType){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver=new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver=new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+            }
+
+        }
+            return driver;
+        }
+
+
+
+
+
+
+
 }
