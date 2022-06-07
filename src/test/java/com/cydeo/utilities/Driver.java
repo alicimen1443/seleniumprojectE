43949209ab754b6f71112516a8041ea7a04ba04f
alicimen <alicimen1443@gmail.com>
2022-06-07 17:25:11 +0300

    day11_ActionsPractices.java

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -15,7 +15,7 @@ public class Driver {
 
      */
 
-    private Driver(){
+    private Driver() {
 
     }
 
@@ -29,9 +29,9 @@ public class Driver {
     Create re-usable utility method which will return same driver instance when we call it
 
      */
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
 
-        if(driver==null){
+        if (driver == null) {
 
             /*
         We read our browserType from configuration.properties.
@@ -43,40 +43,39 @@ public class Driver {
             Depending on the browserType that will be return from configuration.properties file
             switch statement will determine the case, and open the matching browser
              */
-            switch (browserType){
+            switch (browserType) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver=new ChromeDriver();
+                    driver = new ChromeDriver();
                     driver.manage().window().maximize();
                     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
 
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver=new FirefoxDriver();
+                    driver = new FirefoxDriver();
                     driver.manage().window().maximize();
                     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
             }
 
         }
-            return driver;
-        }
-
-
-        //driver.quit()---> nosuchsession
-        //driver.close()-->
-        //try to create a method named closeDriver
-
-   /* public static void closeDriver(){
-        driver.close();
+        return driver;
     }
 
 
-    */
-
-
-
+    //driver.quit()---> nosuchsession
+    //driver.close()-->
+    //try to create a method named closeDriver
+/*
+This method will make sure our driver value is always null after using quit() method
+ */
+    public static void closeDriver() {
+        if(driver!=null){
+            driver.quit(); // this line will terminate the existing session.value will not even be null
+            driver=null;
+        }
+    }
 
 
 }
