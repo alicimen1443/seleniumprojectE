5325afa039611290f3191fdc0a80a6da740994b7
alicimen <alicimen1443@gmail.com>
2022-06-30 00:21:12 +0300

    day13_DynamicControlPage.java

--- src/test/java/com/cydeo/utilities/ReviewUtils.java
+++ src/test/java/com/cydeo/utilities/ReviewUtils.java
@@ -1,7 +1,12 @@
 package com.cydeo.utilities;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReviewUtils {
 
@@ -21,4 +26,29 @@ public class ReviewUtils {
         staticWait(1);
 
     }
+
+    public static double addProduct(WebDriver driver,String category,String product){
+        getLink(driver,category); //click on category
+        getLink(driver,product); //click on product
+        //Let's get products price
+        String priceText = driver.findElement(By.tagName("h3")).getText();
+        double price = Double.parseDouble(priceText.substring(1,4));
+
+        getLink(driver,"Add to cart");
+        Alert alert =driver.switchTo().alert();
+        alert.accept();
+        return price;
+
+    }
+
+    //get List of WebElement as parameter and return list of String
+    public static List<String> getElementsTexts(List<WebElement> inputList){
+        List<String> list = new ArrayList<>();
+        for (WebElement webElement : inputList) {
+            list.add(webElement.getText());
+        }
+        return list;
+    }
+
+
 }
